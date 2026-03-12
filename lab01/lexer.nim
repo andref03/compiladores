36e2fc534b571d709236df44fe8bc658a9b96b8c
Andre Felipe <andref.lopes@outlook.com>
2026-03-12 19:15:09 -0300

    adapting the code to account for symbols and line breaks in tokenization

--- lab01/lexer.nim
+++ lab01/lexer.nim
@@ -1,27 +1,60 @@
 import strutils
 import re
 
-let code = readFile("compil-lab1-amostra-B-Andre-Felipe-de-Oliveira-Lopes.nim")
+var code = readFile("compil-lab1-amostra-B-Andre-Felipe-de-Oliveira-Lopes.nim")
+
+# tratamento de símbolos para garantir que sejam isolados como tokens separados
+# COLON
+for c in [":", ",", ";", "(", ")", "[", "]"]:
+  code = code.replace(c, " " & c & " ")
+
+# NEWLINE
+code = code.replace("\n", " NEWLINE ")
+
+# saída
 let output = open("compil-lab1-resposta-C-Andre-Felipe-de-Oliveira-Lopes.txt", fmWrite)
 
+# laço principal de tokenização
 for token in code.splitWhitespace():
 
-    let cleanToken = token.strip(chars={',',';','(',')','[',']',':'})
+    if token == "NEWLINE":
+        output.writeLine("NEWLINE \\n")
 
-    if cleanToken.match(re"^([0-9]+\.[0-9]*|\.[0-9]+)([eE][+-]?[0-9]+)?$"):
-        output.writeLine("NUM_FLOAT ", cleanToken)
+    elif token == ":":
+        output.writeLine("COLON ", token)
 
-    elif cleanToken.match(re"^[0-9]+([eE][+-]?[0-9]+)?$"):
-        output.writeLine("NUM_INT ", cleanToken)
+    elif token == ",":
+        output.writeLine("COMMA ", token)
 
-    elif cleanToken.match(re"^[a-zA-Z][a-zA-Z0-9]*$"):
-        if cleanToken in ["type", "object", "proc", "if", "int", "elif", "else", "while", "return", "var", "not", "and", "or", "mod"]:
-            output.writeLine("KEYWORD ", cleanToken)
+    elif token == ";":
+        output.writeLine("SEMICOLON ", token)
+
+    elif token == "(":
+        output.writeLine("LPAREN ", token)
+
+    elif token == ")":
+        output.writeLine("RPAREN ", token)
+
+    elif token == "[":
+        output.writeLine("LBRACKET ", token)
+
+    elif token == "]":
+        output.writeLine("RBRACKET ", token)
+
+    elif token == "=":
+        output.writeLine("ASSIGN ", token)
+    
+    elif token.match(re"^([0-9]+\.[0-9]*|\.[0-9]+)([eE][+-]?[0-9]+)?$"):
+        output.writeLine("NUM_FLOAT ", token)
+
+    elif token.match(re"^[0-9]+([eE][+-]?[0-9]+)?$"):
+        output.writeLine("NUM_INT ", token)
+
+    elif token.match(re"^[a-zA-Z][a-zA-Z0-9_]*$"):
+        if token in ["type", "object", "proc", "if", "int", "elif", "else", "while", "return", "var", "not", "and", "or", "mod"]:
+            output.writeLine("KEYWORD ", token)
         else:
-            output.writeLine("ID ", cleanToken)
-
-    elif cleanToken == "=":
-        output.writeLine("ASSIGN ", cleanToken)
+            output.writeLine("ID ", token)
 
 output.close()
 
