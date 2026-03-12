755986e022dff6f83d4804135252a815b33ef684
Andre Felipe <andref.lopes@outlook.com>
2026-03-12 19:27:30 -0300

    adding switch logic and providing basic operators

--- lab01/lexer.nim
+++ lab01/lexer.nim
@@ -17,44 +17,59 @@ let output = open("compil-lab1-resposta-C-Andre-Felipe-de-Oliveira-Lopes.txt", f
 # laço principal de tokenização
 for token in code.splitWhitespace():
 
-    if token == "NEWLINE":
-        output.writeLine("NEWLINE \\n")
+    case token
+        of "NEWLINE":
+            output.writeLine("NEWLINE \\n")
 
-    elif token == ":":
-        output.writeLine("COLON ", token)
+        of ":":
+            output.writeLine("COLON ", token)
 
-    elif token == ",":
-        output.writeLine("COMMA ", token)
+        of ",":
+            output.writeLine("COMMA ", token)
 
-    elif token == ";":
-        output.writeLine("SEMICOLON ", token)
+        of ";":
+            output.writeLine("SEMICOLON ", token)
 
-    elif token == "(":
-        output.writeLine("LPAREN ", token)
+        of "(":
+            output.writeLine("LPAREN ", token)
 
-    elif token == ")":
-        output.writeLine("RPAREN ", token)
+        of ")":
+            output.writeLine("RPAREN ", token)
 
-    elif token == "[":
-        output.writeLine("LBRACKET ", token)
+        of "[":
+            output.writeLine("LBRACKET ", token)
 
-    elif token == "]":
-        output.writeLine("RBRACKET ", token)
+        of "]":
+            output.writeLine("RBRACKET ", token)
 
-    elif token == "=":
-        output.writeLine("ASSIGN ", token)
-    
-    elif token.match(re"^([0-9]+\.[0-9]*|\.[0-9]+)([eE][+-]?[0-9]+)?$"):
-        output.writeLine("NUM_FLOAT ", token)
+        of "=":
+            output.writeLine("ASSIGN ", token)
 
-    elif token.match(re"^[0-9]+([eE][+-]?[0-9]+)?$"):
-        output.writeLine("NUM_INT ", token)
+        of "+":
+            output.writeLine("PLUS ", token)
 
-    elif token.match(re"^[a-zA-Z][a-zA-Z0-9_]*$"):
-        if token in ["type", "object", "proc", "if", "int", "elif", "else", "while", "return", "var", "not", "and", "or", "mod"]:
-            output.writeLine("KEYWORD ", token)
+        of "-":
+            output.writeLine("MINUS ", token)
+
+        of "*":
+            output.writeLine("MUL ", token)
+
+        of "/":
+            output.writeLine("DIV ", token)
+
+        of "%":
+            output.writeLine("MOD ", token)
+
         else:
-            output.writeLine("ID ", token)
+            if token.match(re"^([0-9]+\.[0-9]*|\.[0-9]+)([eE][+-]?[0-9]+)?$"):
+                output.writeLine("NUM_FLOAT ", token)
+            elif token.match(re"^[0-9]+([eE][+-]?[0-9]+)?$"):
+                output.writeLine("NUM_INT ", token)
+            elif token.match(re"^[a-zA-Z][a-zA-Z0-9_]*$"):
+                if token in ["type", "object", "proc", "if", "int", "elif", "else", "while", "return", "var", "not", "and", "or", "mod"]:
+                    output.writeLine("KEYWORD ", token)
+                else:
+                    output.writeLine("ID ", token)
 
 output.close()
 
