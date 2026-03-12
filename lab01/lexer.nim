02b5fab9c309beac6184713bc5f06fe81f5a093a
Andre Felipe <andref.lopes@outlook.com>
2026-03-12 18:35:01 -0300

    writing output on txt archive

--- lab01/lexer.nim
+++ lab01/lexer.nim
@@ -2,22 +2,27 @@ import strutils
 import re
 
 let code = readFile("compil-lab1-amostra-B-Andre-Felipe-de-Oliveira-Lopes.nim")
+let output = open("compil-lab1-resposta-C-Andre-Felipe-de-Oliveira-Lopes.txt", fmWrite)
 
 for token in code.splitWhitespace():
 
     let cleanToken = token.strip(chars={',',';','(',')','[',']',':'})
 
     if cleanToken.match(re"^([0-9]+\.[0-9]*|\.[0-9]+)([eE][+-]?[0-9]+)?$"):
-        echo "NUM_FLOAT ", cleanToken
+        output.writeLine("NUM_FLOAT ", cleanToken)
 
     elif cleanToken.match(re"^[0-9]+([eE][+-]?[0-9]+)?$"):
-        echo "NUM_INT ", cleanToken
+        output.writeLine("NUM_INT ", cleanToken)
 
     elif cleanToken.match(re"^[a-zA-Z_][a-zA-Z0-9_]*$"):
         if cleanToken in ["type", "object", "proc", "if", "int", "elif", "else", "while", "return", "var", "not", "and", "or", "mod"]:
-            echo "KEYWORD ", cleanToken
+            output.writeLine("KEYWORD ", cleanToken)
         else:
-            echo "ID ", cleanToken
+            output.writeLine("ID ", cleanToken)
 
     elif cleanToken == "=":
-        echo "ASSIGN ", cleanToken
+        output.writeLine("ASSIGN ", cleanToken)
+
+output.close()
+
+echo "Tokenização concluída!"
