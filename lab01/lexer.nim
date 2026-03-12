739f252926f4b883535ff404062422a8f0615c7c
andref03 <andref.lopes@outlook.com>
2026-03-11 23:20:22 -0300

    implementing logic to separate keyword and identifiers

--- lab01/lexer.nim
+++ lab01/lexer.nim
@@ -7,6 +7,24 @@ for token in code.splitWhitespace():
     if token.match(re"^[0-9]+$"):
         echo "NUM_DEC ", token
     elif token.match(re"^[a-zA-Z_][a-zA-Z0-9_]*$"):
-        echo "ID ", token
+        if token in ["type", "object", "proc", "if", "int", "elif", "else", "while", "return", "var", "not", "and", "or", "mod"]:
+            echo "KEYWORD ", token
+        else:
+            echo "ID ", token
     elif token == "=":
         echo "ASSIGN ", token
+
+
+# palavras-chave: 
+# type
+# object
+# proc
+# if
+# elif
+# else
+# while
+# return
+# var
+# not
+# and
+# or
