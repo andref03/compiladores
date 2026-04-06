9f9cde2f8ea5aed6840899a242ed06b707cfb40d
Andre Felipe <andref.lopes@outlook.com>
2026-04-05 22:37:54 -0300

    Adjusting to correctly recognize float with exponent and adding $ $ at the end.

--- lab02/compil_lab1.nim
+++ lab02/compil_lab1.nim
@@ -25,7 +25,7 @@ for i, line in originalLines:
     code &= content
 
 # tratamento de símbolos
-for c in [":", ",", ";", "(", ")", "[", "]", "*", "."]:
+for c in [":", ",", ";", "(", ")", "[", "]", "*", ".", "@", "^"]:
     code = code.replace(c, " " & c & " ")
 
 # tratamento de quebras de linha com marcador de indentação
@@ -44,17 +44,32 @@ let output = open("tokens_entrada.txt", fmWrite)
 
 var tokens = code.splitWhitespace()
 
-# recompor números floats (NUM . NUM)
+# recompor números floats e notação científica
 var processedTokens: seq[string] = @[]
 var i = 0
 
 while i < tokens.len:
-    if i + 2 < tokens.len and tokens[i+1] == "." and tokens[i].match(re"^[0-9]+$") and tokens[i+2].match(re"^[0-9]+$"):
+    if i + 2 < tokens.len and
+       tokens[i+1] == "." and
+       tokens[i].match(re"^[0-9]+$") and
+       tokens[i+2].match(re"^[0-9]+([eE][+-]?[0-9]+)?$"):
         processedTokens.add(tokens[i] & "." & tokens[i+2])
         i += 3
-    elif i + 2 < tokens.len and tokens[i] == "." and tokens[i+1].match(re"^[0-9]+$") and tokens[i+2].match(re"^[eE][+-]?[0-9]+$"):
+    elif i + 2 < tokens.len and
+         tokens[i] == "." and
+         tokens[i+1].match(re"^[0-9]+$") and
+         tokens[i+2].match(re"^[eE][+-]?[0-9]+$"):
         processedTokens.add("." & tokens[i+1] & tokens[i+2])
         i += 3
+    elif i + 1 < tokens.len and
+         tokens[i].match(re"^[0-9]+$") and
+         tokens[i+1].match(re"^[eE][+-]?[0-9]+$"):
+        processedTokens.add(tokens[i] & tokens[i+1])
+        i += 2
+    elif tokens[i].match(re"^[+-][a-zA-Z][a-zA-Z0-9_]*$"):
+        processedTokens.add($tokens[i][0])
+        processedTokens.add(tokens[i][1..^1])
+        i += 1
     else:
         processedTokens.add(tokens[i])
         i += 1
@@ -76,8 +91,11 @@ let keywordMap = {
 
     "var": "KEYWORD_VAR",
     "let": "KEYWORD_LET",
+    "inc": "KEYWORD_INC",
+    "dec": "KEYWORD_DEC",
 
     "int": "KEYWORD_INT",
+    "int64": "KEYWORD_INT64",
     "float": "KEYWORD_FLOAT",
     "bool": "KEYWORD_BOOL",
     "string": "KEYWORD_STRING",
@@ -91,7 +109,8 @@ let keywordMap = {
     "not": "KEYWORD_NOT",
     "and": "KEYWORD_AND",
     "or": "KEYWORD_OR",
-    "mod": "KEYWORD_MOD"
+    "mod": "KEYWORD_MOD",
+    "div": "KEYWORD_DIV"
 }.toTable()
 
 # controle de escopo por indentação
@@ -104,7 +123,7 @@ while i < tokens.len:
 
     # tratamento de quebra de linha
     if token == "NEWLINE":
-        output.writeLine("NEWLINE")
+        output.writeLine("NEWLINE \\n")
 
     # tratamento de indentação / escopo
     elif token.startsWith("LINE_INDENT"):
@@ -114,11 +133,11 @@ while i < tokens.len:
 
         if currentIndent > lastIndent:
             indentStack.add(currentIndent)
-            output.writeLine("INDENT")
+            output.writeLine("INDENT INDENT")
         elif currentIndent < lastIndent:
             while indentStack.len > 1 and indentStack[^1] > currentIndent:
                 discard indentStack.pop()
-                output.writeLine("DEDENT")
+                output.writeLine("DEDENT DEDENT")
 
     # tratamento de strings
     elif token.startsWith("\""):
@@ -129,96 +148,109 @@ while i < tokens.len:
             i += 1
             strToken &= " " & tokens[i]
 
-        output.writeLine("STRING")
+        output.writeLine("STRING ", strToken)
 
     else:
 
         case token
         of ":":
-            output.writeLine("COLON")
+            output.writeLine("COLON ", token)
 
         of ",":
-            output.writeLine("COMMA")
+            output.writeLine("COMMA ", token)
 
         of ".":
-            output.writeLine("DOT")
+            output.writeLine("DOT ", token)
+
+        of "@":
+            output.writeLine("AT ", token)
 
         of ";":
-            output.writeLine("SEMICOLON")
+            output.writeLine("SEMICOLON ", token)
 
         of "(":
-            output.writeLine("LPAREN")
+            output.writeLine("LPAREN ", token)
 
         of ")":
-            output.writeLine("RPAREN")
+            output.writeLine("RPAREN ", token)
 
         of "[":
-            output.writeLine("LBRACKET")
+            output.writeLine("LBRACKET ", token)
 
         of "]":
-            output.writeLine("RBRACKET")
+            output.writeLine("RBRACKET ", token)
 
         of "=":
-            output.writeLine("ASSIGN")
+            output.writeLine("ASSIGN ", token)
 
         of "+":
-            output.writeLine("PLUS")
+            output.writeLine("PLUS ", token)
 
         of "-":
-            output.writeLine("MINUS")
+            output.writeLine("MINUS ", token)
 
         of "*":
-            output.writeLine("MUL")
+            output.writeLine("MULT ", token)
+
+        of "^":
+            output.writeLine("CARET ", token)
 
         of "/":
-            output.writeLine("DIV")
+            output.writeLine("DIV ", token)
 
         of "%":
-            output.writeLine("MOD")
+            output.writeLine("MOD ", token)
 
         of "<":
-            output.writeLine("LT")
+            output.writeLine("LT ", token)
 
         of ">":
-            output.writeLine("GT")
+            output.writeLine("GT ", token)
 
         of "<=":
-            output.writeLine("LE")
+            output.writeLine("LE ", token)
 
         of ">=":
-            output.writeLine("GE")
+            output.writeLine("GE ", token)
 
         of "==":
-            output.writeLine("EQ")
+            output.writeLine("EQ ", token)
 
         of "!=":
-            output.writeLine("NE")
+            output.writeLine("NE ", token)
 
         of "+=":
-            output.writeLine("PLUS_ASSIGN")
+            output.writeLine("PLUS_ASSIGN ", token)
 
         of "-=":
-            output.writeLine("MINUS_ASSIGN")
+            output.writeLine("MINUS_ASSIGN ", token)
 
         else:
-            if token.match(re"^([0-9]+\.[0-9]*|\.[0-9]+)([eE][+-]?[0-9]+)?$"):
-                output.writeLine("NUM_FLOAT")
+            if token.match(re"^(([0-9]+\.[0-9]*)|(\.[0-9]+)|([0-9]+))([eE][+-]?[0-9]+)?$") and
+               (token.contains(".") or token.contains("e") or token.contains("E")):
+                output.writeLine("NUM_FLOAT ", token)
 
-            elif token.match(re"^[0-9]+([eE][+-]?[0-9]+)?$"):
-                output.writeLine("NUM_INT")
+            elif token.match(re"^[0-9]+$"):
+                output.writeLine("NUM_INT ", token)
 
             elif token.match(re"^[a-zA-Z][a-zA-Z0-9_]*$"):
                 if token in keywordMap:
-                    output.writeLine(keywordMap[token], "")
+                    if token in ["int", "int64", "float", "bool", "string"] and
+                       i + 1 < tokens.len and tokens[i+1] == "(":
+                        output.writeLine("ID ", token)
+                    else:
+                        output.writeLine(keywordMap[token], " ", token)
                 else:
-                    output.writeLine("ID")
+                    output.writeLine("ID ", token)
 
     i += 1
 
 # fechar escopos restantes no final do arquivo
 while indentStack.len > 1:
     discard indentStack.pop()
-    output.writeLine("DEDENT")
+    output.writeLine("DEDENT DEDENT")
+
+output.writeLine("$ $")
 
 output.close()
 
